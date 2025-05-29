5478ccf23605de12029b14eb6535a7aadaf7ebf9
HazeeraMisman <92832476+HazeeraMisman@users.noreply.github.com>
2025-05-29 14:47:11 +0800

    Update docx_api.py

--- docx_api.py
+++ docx_api.py
@@ -11,6 +11,11 @@ from flask import Flask, request, jsonify
 from docx import Document
 import base64
 import io
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 app = Flask(__name__)
 
